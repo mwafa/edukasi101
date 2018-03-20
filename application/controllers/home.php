4154970360ddf4267865ddc83e11cc0d6e173b96
mwafa <wafax.4@gmail.com>
2018-03-21 00:36:59 +0700

    editing configuration

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class home extends CI_Controller {
 	public function index()
 	{
-		header("location: /login");
-		$this->load->view('welcome_message');
+		redirect(base_url('login'));
+		
 	}
 }
